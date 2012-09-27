e1d72c76c0fbafe84b075053afa843176dc77f90
Federico Spini <federico.spini@gmail.com>
2012-09-27 17:06:27 +0200

    added combine

--- test/op.js
+++ test/op.js
@@ -36,39 +36,9 @@ describe('op', function () {
     });
   });
 
-  describe('extrude', function () {
-    it('should return -1 when the value is not present', function () {
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
+  describe('extrude', function () { ; });
 
-    it('should return -1 when the value is not present', function () {
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-  });
+  describe('combine', function () { ; });
 
-  describe('combine', function () {
-    it('should return -1 when the value is not present', function () {
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-
-    it('should return -1 when the value is not present', function () {
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-  });
-
-  describe('extract0', function () {
-    it('should return -1 when the value is not present', function () {
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-
-    it('should return -1 when the value is not present', function () {
-      [1,2,3].indexOf(5).should.equal(-1);
-      [1,2,3].indexOf(0).should.equal(-1);
-    });
-  });
+  describe('extract0', function () { ; });
 });
