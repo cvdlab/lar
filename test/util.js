a604b5c9bcf876ffecd2aade3251aec81fb6609d
Federico Spini <federico.spini@gmail.com>
2012-09-29 13:23:16 +0200

    added Topology constructor tests

--- test/util.js
+++ test/util.js
@@ -3,11 +3,23 @@ var lar = require('../lib/lar.js').lar;
 
 describe('utils', function () {
   describe('_binarize', function () {
-    it('should binarize matrix', function () { });
+    it('should binarize matrix', function () { 
+      var matrix = [[0,1,3,4],[1,2,3,4]];
+      var expectedMatrix =  [[1,1,0,1,1],[0,1,1,1,1]];
+      var calculatedMatrix = lar._private.binarize(matrix);
+
+      calculatedMatrix.should.eql(expectedMatrix);
+    });
   });
 
   describe('_unbinarize', function () {
-    it('should unbinarize matrix', function () { });
+    it('should unbinarize matrix', function () { 
+      var matrix = [[1,0,0,0,0,0,1],[0,1,0,0,1,0,0],[0,0,1,1,0,0,0],[0,0,0,1,1,0,0],[0,0,0,0,1,1,0]];
+      var expectedMatrix =  [[0,6],[1,4],[2,3],[3,4],[4,5]];
+      var calculatedMatrix = lar._private.unbinarize(matrix);
+
+      calculatedMatrix.should.eql(expectedMatrix);
+    });
   });
 
   describe('_unbinarize(_binarize((matrix))', function () {
