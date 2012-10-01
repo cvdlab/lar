e0ddf534752cdbbb56aff8434184c92ef212e9ab
Federico Spini <federico.spini@gmail.com>
2012-10-01 17:48:22 +0200

    fixed api

--- test/model.js
+++ test/model.js
@@ -4,9 +4,26 @@ var lar = require('../lib/lar.js').lar;
 
 describe('Model', function () {
   describe('boundary', function () {
-    it('should extract boundary of a Model', function () {
+    it('should extract boundary of a 2d Model', function () {
       var cells = [[0,1,2],[1,2,3]];
       var vertices = [[0,0],[0,1],[0.5,1],[1.5,1]];
+      var m = new lar.Model(vertices, cells);
+      var boundary = m.boundary(1, [0,1]);
+      var t = boundary.topology;
+      
+      var expected_c10 = [[0,1,1,0],[0,1,0,1],[0,0,1,1]];
+      var expected_c01 = numeric.transpose(expected_c10);
+      var expected_c00 = lar.ops.extract0(expected_c10);
+
+      t.get(1,0).should.eql(expected_c10);
+      t.get(0,1).should.eql(expected_c01);
+      t.get(0,0).should.eql(expected_c00);
+      vertices.should.eql(vertices);
+    });
+
+    it('should extract boundary of a 3d Model', function () {
+      var cells = [[0,1,2,3],[1,2,3,4]];
+      var vertices = [[0,0,5],[0,0,0],[0,2,0],[3,1,0]];
       var m = new lar.Model(vertices, cells);
       var boundary = m.boundary(1, [0,1]);
       var t = boundary.topology;
