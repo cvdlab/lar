f27ba785736465d457cb248b3d85b56b15e69c6f
Federico Spini <federico.spini@gmail.com>
2012-10-02 10:43:23 +0200

    fixed api

--- test/model.js
+++ test/model.js
@@ -3,6 +3,28 @@ var numeric = require('numeric');
 var lar = require('../lib/lar.js').lar;
 
 describe('Model', function () {
+   describe('getCells', function () {
+    it('should extract a 1-chain from a 2d Model', function () {
+      var cells = [[0,4,2],[4,2,3],[2,3,1]];
+      var vertices = [[0,0],[2,0],[3,1],[4,0],[1,1]];
+      var m = new lar.Model(vertices, cells);
+      var d_cells = m.getCells(1);
+      var expectedCells = [[0,2],[0,4],[1,2],[1,3],[2,3],[2,4],[3,4]];
+      expectedCells.should.eql(d_cells);
+    });
+  });
+
+   describe('getChain', function () {
+    it('should extract a 1-chain from a 2d Model', function () {
+      var cells = [[0,4,2],[4,2,3],[2,3,1]];
+      var vertices = [[0,0],[2,0],[3,1],[4,0],[1,1]];
+      var m = new lar.Model(vertices, cells);
+      var d_chain = m.getChain(1,[0,1,1,0,1,0,0]);
+      var expectedChain = [[0,4],[1,2],[2,3]];
+      expectedChain.should.eql(d_chain);
+    });
+  });
+
   describe('boundary', function () {
     it('should extract boundary of a 2d Model', function () {
       var cells = [[0,1,2],[1,2,3]];
@@ -23,9 +45,9 @@ describe('Model', function () {
 
     it('should extract boundary of a 3d Model', function () {
       var cells = [[0,1,2,3],[1,2,3,4]];
-      var vertices = [[0,0,5],[0,0,0],[0,2,0],[3,1,0]];
+      var vertices = [[2.5,2.5,5],[0,0,0],[5,0,0],[5,5,0],[2.5,2.5,-5]];
       var m = new lar.Model(vertices, cells);
-      var boundary = m.boundary(1, [0,1]);
+      var boundary = m.boundary(2, [0,1]);
       var t = boundary.topology;
       
       var expected_c10 = [[0,1,1,0],[0,1,0,1],[0,0,1,1]];
