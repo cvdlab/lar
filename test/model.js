74b320e05e750430056820a69e20dd8b31a0ae4b
Federico Spini <federico.spini@gmail.com>
2012-10-02 12:07:58 +0200

    fixed api

--- test/model.js
+++ test/model.js
@@ -43,21 +43,25 @@ describe('Model', function () {
       vertices.should.eql(vertices);
     });
 
-    it('should extract boundary of a 3d Model', function () {
+    it('should extract 1-boundary of a chain from 3d Model', function () {
       var cells = [[0,1,2,3],[1,2,3,4]];
       var vertices = [[2.5,2.5,5],[0,0,0],[5,0,0],[5,5,0],[2.5,2.5,-5]];
       var m = new lar.Model(vertices, cells);
-      var boundary = m.boundary(2, [0,1]);
-      var t = boundary.topology;
-      
-      var expected_c10 = [[0,1,1,0],[0,1,0,1],[0,0,1,1]];
-      var expected_c01 = numeric.transpose(expected_c10);
-      var expected_c00 = lar.ops.extract0(expected_c10);
+      var b = m.boundary(1, [0,0,0,1,0,0,0]);
+      var boundaryCells = b.getCells(1);
+      var expectedboudnaryCells = [[1,2],[1,3],[2,3]];
 
-      t.get(1,0).should.eql(expected_c10);
-      t.get(0,1).should.eql(expected_c01);
-      t.get(0,0).should.eql(expected_c00);
-      vertices.should.eql(vertices);
+      boundaryCells.should.eql(expectedboudnaryCells);
+    });
+
+    it('should give an empty border when query boundary(boundary(model))', function () {
+      var cells = [[0,1,2,3],[1,2,3,4]];
+      var vertices = [[2.5,2.5,5],[0,0,0],[5,0,0],[5,5,0],[2.5,2.5,-5]];
+      var m = new lar.Model(vertices, cells);
+      var b1 = m.boundary(2);
+      var b2 = b1.boundary(1);
+
+      b2.isEmpty().should.be.true;
     });
   });
 });
